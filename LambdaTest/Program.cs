d49e7718186abf9e1634e7522bc9ab103a9662e1
Bartek <bartlomiej.kacki@gmail.com>
2024-08-07 20:48:44 +0200

    Add project files.

---
+++ LambdaTest/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace LambdaTest
+{
+    internal class Program
+    {
+        static Random random => new Random();
+
+        static double GetRandomDouble(int max) => max*random.NextDouble();
+
+        static void PrintValue(double d) => Console.WriteLine($"Wartość wynosi {d:N2}.");
+
+        static void Main(string[] args)
+        {
+            var value = Program.GetRandomDouble(100);
+            string sformatowanaLiczba = string.Format("{0:N2}", value);
+            Console.WriteLine($"{value:F2}");
+            Console.WriteLine(sformatowanaLiczba);
+        }
+    }
+}
